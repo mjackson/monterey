fc709a3c842b02538f8c2ee739d66edd33136767
Michael Jackson <mjijackson@gmail.com>
2014-12-18 16:56:46 -0800

    Move browser tests

--- karma.conf.js
+++ karma.conf.js
@@ -6,11 +6,11 @@ module.exports = function (config) {
     frameworks: [ 'mocha' ],
 
     files: [
-      'modules/__tests__/browser.js'
+      'tests.webpack.js'
     ],
 
     preprocessors: {
-      'modules/__tests__/browser.js': [ 'webpack', 'sourcemap' ]
+      'tests.webpack.js': [ 'webpack', 'sourcemap' ]
     },
 
     webpack: {
